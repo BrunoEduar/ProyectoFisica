d5bda19d348b491141437b8ca5aff1ddb95b2e83
BrunoEduar <104513620+BrunoEduar@users.noreply.github.com>
2023-02-24 00:50:21 -0600

    Corrección de errores (1)
    Se han quitado errores básicos para poder compilar.

--- ProyectoFisica.java
+++ ProyectoFisica.java
@@ -10,7 +10,7 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class Practica4Asegurador extends JFrame implements ActionListener {
+public class ProyectoFisica extends JFrame implements ActionListener {
 
     /**
 *
@@ -18,11 +18,11 @@ public class Practica4Asegurador extends JFrame implements ActionListener {
 private static final long serialVersionUID = 1L;
 private JLabel etiquetaPregunta1, etiquetaPregunta2, etiquetaResultado;
     private JTextField campoTextoResultado,campoPregunta1, campoPregunta2;
-    private JButton botonCerrar, botonLimpiar, botonAceptar, botonRepetir;
+    private JButton botonCerrar, botonLimpiar, botonAceptar;
    
 
     public static void main(String[] args) {
-        Practica4Asegurador demo = new Practica4Asegurador ();
+        ProyectoFisica demo = new ProyectoFisica ();
         demo.setSize(800,800);
         demo.crearGUI();
         demo.setTitle("ProyectoFisica");
